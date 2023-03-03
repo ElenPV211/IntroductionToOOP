73773ed64c33f1b517c52e6e649fbee3b159b0e7
user <user@DESKTOP-LI69LEU>
2023-03-03 20:57:50 +0300

    Methotds out of class

--- String/main.cpp
+++ String/main.cpp
@@ -9,115 +9,123 @@ class String
 	int size;	//размер строки в Байтах
 	char* str;	//указатель на строку в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
-	//				Constructors:
-	explicit String(int size = 80):size(size), str(new char[size]{})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "DefConstructor:\t" << this << endl;
-	}
-	String(const char* str):String(strlen(str)+1)
-	{
-		//this->size = strlen(str)+1;//+1для NULL - terminatora
 
-		//this->str = new char[size] {};
+	int get_size()const;//заголовки методов
+	const char* get_str()const;
+	char* get_str();
 
-		for (int i = 0; str[i]; i++)this->str[i] = str[i];
-		
-		cout << "ConstructorChar:\t" << this << endl;
-	
-	}
-	String(const String& other):String(other.str)
-	{
-		//this->size = other.size;
-		//Deep copy (побитовое копирование)
+	//---------------Constructors:-----------------
+	explicit String(int size = 80);
+	String(const char* str);
+	//Deep copy (побитовое копирование)
+	String(const String& other);
 
-		//this->str = new char[size] {};
-		
-		cout << "CopyConstructor:" << this << endl;
-	}
+	String(String&& other);
+	~String();
+	//---------------Operator:----------------------
 
-	String(String&& other):size(other.size), str(other.str)
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
+	String& operator=(const String& other);
+	char operator[](int i) const;
+	char& operator[](int i);
 
-	{
-		//this->size = other.size;
-		//this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveConstructor:" << this << endl;
-
-	}
-	~String()
-	{
-		delete[] this->str;
-		this->str = nullptr;
-		cout << "Destructor:\t" << this << endl;
-	}
-	// Operator:
-
-	String& operator=(String&& other) 
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout <<"MoveAssignment:\t"<< this << endl;
-		return *this;
-	}
-
-	String& operator+=(const String& other)
-	{
-		
-			return *this = *this + other; // обращение к функции 
-		}
-
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;// если наше значение = тому значению
-		//то сразу выходим из оператора присваивания чтобы данные не обнулились
-		delete[]this->str; 
-
-		this->size = other.size;
-		//Deep copy (побитовое копирование)
-
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-			this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	char operator[](int i) const
-	{
-		return str[i];
-	}
-char& operator[](int i)
-	{
-		return str[i];
-	}
-
-	//				Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-
-		cout << "Str:\t" << str << endl;
-	}
+	//---------------Methods:------------------------
+	void print()const;
 };
 
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+//----------------Constructors:-----------------------
+String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "DefConstructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	for (int i = 0; str[i]; i++)this->str[i] = str[i];
+
+	cout << "ConstructorChar:\t" << this << endl;
+
+}//Deep copy (побитовое копирование)
+String::String(const String& other) :String(other.str)
+{
+	cout << "CopyConstructor:" << this << endl;
+}
+
+String::String(String&& other) :size(other.size), str(other.str)
+
+{
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;
+	this->str = nullptr;
+	cout << "Destructor:\t" << this << endl;
+}
+//---------------------Operator:-----------------------------
+
+String& String::operator=(String&& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;// если наше значение = тому значению
+	//то сразу выходим из оператора присваивания чтобы данные не обнулились
+	delete[]this->str;
+
+	this->size = other.size;
+	//Deep copy (побитовое копирование)
+
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)
+		this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator+=(const String& other)
+{
+
+	return *this = *this + other; // обращение к функции 
+}
+
+char String::operator[](int i) const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//-------------------Methods:-----------------------------
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
