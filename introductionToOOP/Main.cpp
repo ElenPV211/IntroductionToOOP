b90c52df568e589e706b72a3904bf69b1b080517
user <user@DESKTOP-LI69LEU>
2023-03-05 12:08:10 +0300

    Methods out of class point

--- introductionToOOP/Main.cpp
+++ introductionToOOP/Main.cpp
@@ -1,66 +1,123 @@
 #include<iostream>
 using namespace std;
 
+
+//////////////////////////////////////////////////////////////////
+//////------CLASS DECLARATION------//////////////////////////////
 class Point
 {
-	//Создавая структуру или класс мы создаем новый тип данных
-	//Классы и структуры еще называют пользовательскими типами данных
-	//						КЛАСС - ЭТО ТИП ДАННЫХ!!!
-	//					СТРУКТУРА - ЭТО ТИП ДАННЫХ!!!
-
 	double x;
 	double y;
 public:
-	double get_x() const
-	{
-		return x;
-	}
-	double get_y() const
-	{
-		return y;
-	}
-	void set_x(double x) //обычно set ничего не возвращают поэтому void
-	{
-		this->x = x;
-	}
-	void set_y(double y)
-	{
-		this->y = y;
-	}
-	//  Constructors:
-	/*Point()
-	{
-		//cout << double() << endl;
-		x = y = double();
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	Point(double x)
-	{
-		this->x = x;
-		this->y = 0;
-		cout << "1ArgConstructor:\t" << this << endl;
-	}*/
-	Point(double x = 0, double y = 0)
-	{
-		this->x = x;
-		this->y = y;
-		cout << "Constructor:\t\t" << this << endl;
-	}
-	~Point()
-	{
-		cout << "Destructor:\t" << this << endl;
-	}
+	double get_x() const;
+	double get_y() const;
+	void set_x(double x);
+	void set_y(double y);
+
+	//----------Constructors:------------------------
 	
+	Point(double x = 0, double y = 0);
+	Point(const Point& other);
+	~Point();
 
+	//-------------Operators:-------------//
 
-	//Metods:
-	void print()const
-	{
-		cout << "X = " << x << "\tY = " << y << endl;
-	}
+	Point& operator = (const Point& other);
+	double distance(const Point& other)const;
+
+	//----------Metods:------------------
+	void print()const;
+
 };
+////////////----END CLASS DECLARATION---/////////////
+/////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////
+///////////----ОПРЕДЕЛЕНИЕ КЛАССА - CLASS DEFINITION ///
+
+double Point::get_x() const
+{
+	return x;
+}
+double Point::get_y() const
+{
+	return y;
+}
+void Point::set_x(double x) //обычно set ничего не возвращают поэтому void
+{
+	this->x = x;
+}
+void Point::set_y(double y)
+{
+	this->y = y;
+}
+
+//----------Constructors:------------------------
+
+Point::Point(double x, double y) :x(x), y(y)
+{
+	cout << "Constructor:\t\t" << this << endl;
+}
+
+Point::Point(const Point& other) :x(other.x), y(other.y)
+{
+	cout << "CopyConstructor:\t" << this << endl;
+}
+
+Point::~Point()
+{
+	cout << "Destructor:\t" << this << endl;
+}
+
+//-------------Operators:-------------//
+
+Point& Point::operator = (const Point& other)
+{
+	this->x = other.x;
+	this->y = other.y;
+	cout << "CopyAssignment:\t\t" << this << endl;
+	return *this;
+}
+double Point::distance(const Point& other)const//const делает this недоступным для изменения
+{
+	double x_distance = this->x - other.x;
+	double y_distance = this->y - other.y;
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
+
+///////----------Metods:------------------//////////
+
+void Point::print()const
+{
+	cout << "X = " << x << "\tY = " << y << endl;
+}
+
+Point operator+(const Point& left, const Point& right) 
+{
+	Point res;
+	res.set_x(left.get_x() + right.get_x());
+	res.set_y(left.get_y() + right.get_y());
+	return res;
+}
+
+double distance(const Point& A, const Point& B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
+////////////////////КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАССА - CLASS DEFINITION END /////
+/////////////////////////////////////////////////////////////////////
+
 
 //#define STRUCT_POINT //имя макроса
+//#define CONSTRUCTOR_CHECK
+//#define DISTANCE_CHECK
+//#define ASSIGMENT_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -79,6 +136,7 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif //STRUCT_POINT
 
+#ifdef CONSTRUCTOR_CHECK
 	Point A; //Здесь вызывается конструктор по умолчанию (Default
 	//A.set_x(2);
 	//A.set_y(3);
@@ -89,7 +147,53 @@ void main()
 
 	Point C(22, 33);
 	C.print();
+	Point D = C;
+	D.print();
+
+	Point E;
+	E = D; //Copy assignment
+	E.print();
+
+#endif CONSTRUCTOR_CHECK
+#ifdef DISTANCE_CHECK
+	Point A(2, 3);
+	A.print();
+	Point B(4, 5);
+	B.print();
+	cout << delimiter << endl;
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки В до точки А: " << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками А и В: " << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками B и A: " << distance(B, A) << endl;
+	cout << delimiter << endl;
+#endif //DISTANCE_CHECK
+
+#ifdef ASSIGMENT_CHECK
+	//Copy assigment:
+	int a, b, c;
+	a = b = c = 2;
+	std::cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+	B.print();
+	C.print();
+#endif ASSIGMENT_CHECK
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3); A.print();
+	Point B(4, 5); B.print();
+	Point C = A + B; C.print();
+
 	}
+
+
 
 
 /*
